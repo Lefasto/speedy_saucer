82d6e1502fa63afd18173fb1a027c03b49c477ae
Lefasto <g.palfoeldi@gmail.com>
2025-08-24 22:04:26 +0200

    add player input

--- player.gd
+++ player.gd
@@ -1,12 +1,12 @@
 extends RigidBody2D
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass
-
-func _process(delta: float) -> void:
-	pass
-
 func _physics_process(delta: float) -> void:
-	apply_force(Vector2(25, 0))
+	var force: int = 500
+	if Input.is_action_pressed("move_right"):
+		apply_force(Vector2(force, 0))
+	if Input.is_action_pressed("move_left"):
+		apply_force(Vector2(-force, 0))
+	if Input.is_action_pressed("move_up"):
+		apply_force(Vector2(0, -force))
+	if Input.is_action_pressed("move_down"):
+		apply_force(Vector2(0, force))
